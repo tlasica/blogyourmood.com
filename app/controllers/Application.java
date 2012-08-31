e2be8ae716b3e59582476989f9057a5e04961cb5
Tomasz Łasica <tlasica@o2.pl>
2012-08-31 23:48:20 +0200

    submit button via css. blog entity. create new blog action works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,17 +2,39 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
 
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
+  
+  static Form<Blog> blogCreateForm = form(Blog.class);
   
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
   
   public static Result register() {
-	return ok(register.render());
+	return ok(register.render(blogCreateForm));
+  }
+
+  public static Result createBlog() {
+	Form<Blog> form = blogCreateForm.bindFromRequest();
+	if ( form.hasErrors() ) {
+		return badRequest( register.render(form) );
+	} 
+	else {
+		Blog blog = form.get();
+		blog = Blog.create(blog);
+		return redirect(routes.Application.blog(blog.privateLink));		
+	}
+  }
+  
+  public static Result blog(String privateLink) {
+	Blog blog = Blog.findByPrivateLink(privateLink);
+	//TODO: load all requred data - list of last 3
+	return ok(views.html.blog.render(blog));
   }
   
 }
