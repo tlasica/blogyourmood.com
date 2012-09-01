f3412bfc762102cd11fcbc98459a9233a78f1c01
Tomasz Łasica <tlasica@o2.pl>
2012-09-01 13:46:01 +0200

    added postgressql driver dependency

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+		"postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+		// Add your project dependencies here,
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
