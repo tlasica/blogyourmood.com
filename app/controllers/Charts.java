4c0afeecfc1ea9dd5b6a74edf9019484af534a93
Tomasz Łasica <tlasica@o2.pl>
2012-09-04 00:47:36 +0200

    TODO instead of NO ACTION. Current mood border ugly way (brrrr)

---
+++ app/controllers/Charts.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+
+public class Charts extends Controller {
+
+	  public static Result timechart(String blogPrivLink, String days) {
+		  return TODO;
+	  }
+
+	  public static Result histogram(String blogPrivLink, String days) {
+		  return TODO;
+	  }
+
+	  public static Result calendar(String blogPrivLink, String period) {
+		  return TODO;
+	  }
+
+}
