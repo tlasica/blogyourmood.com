8a6f9614bca279c55b0ca01cf0a46b5d6a764be6
Tomasz Łasica <tlasica@o2.pl>
2012-09-07 22:51:08 +0200

    Fading message added to blog after save. Remove underlining current mood. Thighten calendar row

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,67 +12,67 @@ import models.*;
 //TODO: match instead of ifs in blog.scala.html
 
 public class Application extends Controller {
-  
+
 	private static final int DEF_HISTORY_LIMIT = 3;
-	private static final String DEMO_BLOG_LINK = "demo123";	
-  
+	private static final String DEMO_BLOG_LINK = "demo123";
+
 	static Form<Blog> blogCreateForm = form(Blog.class);
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
-  public static Result register() {
-	return ok(register.render(blogCreateForm));
-  }
 
-  public static Result createBlog() {
-	Form<Blog> form = blogCreateForm.bindFromRequest();
-	if ( form.hasErrors() ) {
-		return badRequest( register.render(form) );
-	} 
-	else {
-		Blog blog = form.get();
-		blog.generateLinks();
-		blog = Blog.saveNewBlog(blog);
-		return redirect(routes.Application.blog(blog.privateLink));		
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
 	}
-  }
-  
-  public static Result blog(String privateLink) {
-	Blog blog = Blog.findByPrivateLink(privateLink);
-	List<BlogEntry> blogHistory = null;
-	blogHistory = BlogEntry.loadBlogHistory(blog, DEF_HISTORY_LIMIT);
-	String currentMood = "?";
-	if (!blogHistory.isEmpty()) {
-		currentMood = blogHistory.get(0).mood.toString();
+
+	public static Result register() {
+		return ok(register.render(blogCreateForm));
 	}
-	return ok(views.html.blog.render(blog, blogHistory, currentMood));
-  }
-  
-  public static Result addStatus(String blogPrivLink, String mood) {
-	  BlogEntry.saveCurrentMoodInBlog(blogPrivLink, mood);
-	  return redirect(routes.Application.blog(blogPrivLink));		
-  }
 
-  public static Result blogPublicView(String publicLink) {
-	  return TODO;
-  }
+	public static Result createBlog() {
+		Form<Blog> form = blogCreateForm.bindFromRequest();
+		if (form.hasErrors()) {
+			return badRequest(register.render(form));
+		} else {
+			Blog blog = form.get();
+			blog.generateLinks();
+			blog = Blog.saveNewBlog(blog);
+			return redirect(routes.Application.blog(blog.privateLink));
+		}
+	}
 
-  public static Result demo() {
+	public static Result blog(String privateLink) {
+		return showBlog(privateLink, null);
+	}
+
+	public static Result showBlog(String privateLink, String message) {
+		Blog blog = Blog.findByPrivateLink(privateLink);
+		List<BlogEntry> blogHistory = null;
+		blogHistory = BlogEntry.loadBlogHistory(blog, DEF_HISTORY_LIMIT);
+		return ok(views.html.blog.render(blog, blogHistory, message));		
+	}
+	
+	public static Result addStatus(String blogPrivLink, String mood) {
+		BlogEntry.saveCurrentMoodInBlog(blogPrivLink, mood);		
+		String message = "Your mood has been saved.";
+		return showBlog(blogPrivLink, message); 
+	}
+
+	public static Result blogPublicView(String publicLink) {
+		return TODO;
+	}
+
+	public static Result demo() {
 		createDemoBlogIfNotExists();
 		return blog(DEMO_BLOG_LINK);
 	}
-  
-  public static void createDemoBlogIfNotExists() {
+
+	public static void createDemoBlogIfNotExists() {
 		Blog blog = Blog.findByPrivateLink(DEMO_BLOG_LINK);
-		if (null==blog) {
+		if (null == blog) {
 			blog = new Blog();
 			blog.label = "Demo Version - try it!";
 			blog.privateLink = DEMO_BLOG_LINK;
-			blog.publicLink = DEMO_BLOG_LINK+"public";
+			blog.publicLink = DEMO_BLOG_LINK + "public";
 			Blog.saveNewBlog(blog);
-		}		
-  }
-  
+		}
+	}
+
 }
